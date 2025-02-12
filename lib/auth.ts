212f8e200ff0b435ab5c14614542f1bf14acb260
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-02-12 01:12:27 -0500

    Create NextAuthOptions file & Create "Providers" Option

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider  from "next-auth/providers/credentials";
+
+ export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+
+            },
+            async authorize(){
+
+            }
+        }) 
+    ]
+ } 
