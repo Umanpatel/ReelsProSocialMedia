a8c44d9f64d9a07d11b48b090e6b55987b96bceb
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-02-14 14:54:02 -0500

    Create Imagekit Providers, Add Authenticator Method

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,34 +1,35 @@
-// "use client";
-// import React from "react";
-// import { ImageKitProvider } from "imagekitio-next";
+"use client";
+import React from "react";
+import { ImageKitProvider } from "imagekitio-next";
 
-// const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
-// const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-// const authenticator = async () => {
-//   try {
-//     const response = await fetch("/api/imagekit-auth");
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const authenticator = async () => {
+  try {
+    const response = await fetch("/api/imagekit-auth");
 
-//     // if response ok nathi to throw error
-//     if (!response.ok) {
-//       const errorText = await response.text();
-//       throw new Error(`Request failed with status ${response.status}: ${errorText}`);
-//     }
+    // if response ok nathi to throw error
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+    }
 
-//     const data = await response.json();
-//     const { signature, expire, token } = data;
-//     return { signature, expire, token };
-//   } catch (error) {
-//     console.log(error)
-//     throw new Error(`Imagekit Authentication request failed!!!`);
-//   }
-// };
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error) {
+    console.log(error)
+    throw new Error(`Imagekit Authentication request failed`);
+  }
+};
 
-// export default function Home() {
-//   return (
-//       <ImageKitProvider 
-//         urlEndpoint={urlEndpoint} 
-//         publicKey={publicKey} 
-//         authenticator={authenticator}>
-//       </ImageKitProvider>
-//   );
-// }
+export default function Home() {
+  return (
+    <div className="App">
+      <ImageKitProvider urlEndpoint={urlEndpoint} publicKey={publicKey} authenticator={authenticator}>
+        {/* ...client side upload component goes here */}
+      </ImageKitProvider>
+      {/* ...other SDK components added previously */}
+    </div>
+  );
+}
